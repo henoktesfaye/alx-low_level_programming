cdce2f1bbd8048c0391c2964500767aea5c19c42
henoktesfaye <samitesfaye369@gmail.com>
2023-01-11 10:27:53 -0800

    nw

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include"main.h"
+
+/**
+ * main - print name of the program
+ * @argc: count arg
+ * @argv: argument
+ * Return: null
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", args - 1);
+	(void)argv;
+	return (0);
+}
