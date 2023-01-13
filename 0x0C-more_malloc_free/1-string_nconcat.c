d2b7214da9e7d6ed17710f5fa9993ea541dfef7c
henoktesfaye <samitesfaye369@gmail.com>
2023-01-13 08:42:33 -0800

    new

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,40 +1,38 @@
-#include"main.h"
 #include<stdlib.h>
+#include"main.h"
 
 /**
- * string_nconcat - concatenate two stin
- * @s1: first string
- * @s2: second strign
- * @n: amount of bytes
- * Return: pointer to allocated mem
+ * *string_nconcat - concatenates n bytes of a string
+ * @s1: string to append
+ * @s2:string to conc
+ * @n: number of bytes
+ * Return: pointers
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *sout;
+	char *s;
 
-	Unsigned int x, y, z, i;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	for (x = 0; s1[x] != '\0'; x++)
-		;
-	for (y = 0; s2[y] != '\0'; y++)
-		;
-	if (n > y)
-		n = y;
-	z = x + n;
-	sout = malloc(z + 1);
-	if (sout == NULL)
+	while (s1 && s1[len1])
+		len1++;
+	while (s2 && s2[len2])
+		len2++;
+	if (n < len2)
+		s = malloc(sizeof(char) * (len1 + n + 1));
+	else
+		s = malloc(sizeof(char) * (len1 + len2 + 1));
+	if (!s)
 		return (NULL);
-	for (i = 0 i < z; i++)
+	while (i < len1)
 	{
-		if (i < x)
-			sout[i] = s1[i];
-		else
-			sout[i] = s2[i - x];
+		s[i] = s1[i];
+		i++;
 	}
-	sout[i] = '\0';
-	return (sout);
+	while (n < len2 && i < (len1 + n))
+		s[i++] = s2[j++];
+	while (n >= len2 && i < (len1 + len2))
+		s[i++] = s2[j++];
+	s[i] = '\0';
+	return (s);
 }
